463f7c02437ed145f1cb49ed46a86a0e3fbaace5
Victoria Sánchez <victoria.sanchez@utp.edu.co>
2025-01-22 22:32:09 -0500

    Preparing SQL

--- wp-content/themes/inventory-theme/index.php
+++ wp-content/themes/inventory-theme/index.php
@@ -1,5 +1,29 @@
 <!DOCTYPE html>
-<html <?php language_attributes(); ?>>
+<html <?php language_attributes(); ?>> 
 
-
+<head>
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <title><?php bloginfo('name'); ?></title>
+    <?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
+    <header>
+        <h1><?php bloginfo('name'); ?></h1>
+        <nav><?php wp_nav_menu(); ?></nav>
+    </header>
+    <main>
+        <h2>tu mejor activo es tu mente y  nadie puede te lo puede quitar./h2>
+        <?php
+        if ( have_posts() ) {
+            while ( have_posts() ) {
+                the_post();
+                the_title('<h3>', '</h3>');
+                the_content();
+            }
+        }
+        ?>
+    </main>
+    <?php wp_footer(); ?>
+</body>
 </html>
+
