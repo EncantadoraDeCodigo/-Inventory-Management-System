1a68b83c97606a6bfec1450974bdb6014ef96395
Victoria Sánchez <victoria.sanchez@utp.edu.co>
2025-01-29 22:24:18 -0500

    Dashboard request to data base

--- wp-content/themes/inventory-theme/functions.php
+++ wp-content/themes/inventory-theme/functions.php
@@ -3,7 +3,7 @@
 function inventory_enqueue_assets() {
     // Cargar estilos
     wp_enqueue_style('bootstrap-css', 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css');
-    wp_enqueue_style('theme-style', get_stylesheet_uri());
+    wp_enqueue_style('custom-style', get_stylesheet_uri());
 
     // Cargar scripts
     wp_enqueue_script('bootstrap-js', 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js', array('jquery'), null, true);
