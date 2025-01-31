6c85a5b390dcd37bfb58fd2245b9fa3bf6f8484a
Victoria Sánchez <victoria.sanchez@utp.edu.co>
2025-01-30 22:26:51 -0500

    dashboard strusture

--- wp-content/themes/inventory-theme/templates/page-dashboard.php
+++ wp-content/themes/inventory-theme/templates/page-dashboard.php
@@ -2,12 +2,12 @@
     <h1 class="mb-4">Dashboard</h1>
     <div class="row">
         <?php
-        global $wpdb;
-        $metrics = [
-            ['title' => 'Clientes', 'count' => $wpdb->get_var("SELECT COUNT(*) FROM wp_clients"), 'color' => 'success', 'icon' => 'fas fa-users'],
-            ['title' => 'Proveedores', 'count' => $wpdb->get_var("SELECT COUNT(*) FROM wp_providers"), 'color' => 'warning', 'icon' => 'fas fa-truck'],
-            ['title' => 'Productos', 'count' => $wpdb->get_var("SELECT COUNT(*) FROM wp_products"), 'color' => 'primary', 'icon' => 'fas fa-box'],
-            ['title' => 'Facturas', 'count' => $wpdb->get_var("SELECT COUNT(*) FROM wp_invoices"), 'color' => 'dark', 'icon' => 'fas fa-file-invoice'],
+        global $wpdb; //a object declarated in global way to interact with the data base
+        $metrics = [ //a variable declared to save all of the information
+            ['title' => 'Clientes', 'count' => $wpdb->get_var("SELECT COUNT(*) FROM clients"), 'color' => 'success', 'icon' => 'bi bi-person-workspace'], //It'll count the rows of each table, it means the number of clients in this case.
+            ['title' => 'Proveedores', 'count' => $wpdb->get_var("SELECT COUNT(*) FROM providers"), 'color' => 'warning', 'icon' => 'fas fa-truck'],
+            ['title' => 'Productos', 'count' => $wpdb->get_var("SELECT COUNT(*) FROM products"), 'color' => 'primary', 'icon' => 'fas fa-box'],
+            ['title' => 'Facturas', 'count' => $wpdb->get_var("SELECT COUNT(*) FROM invoices"), 'color' => 'dark', 'icon' => 'fas fa-file-invoice'],
             ['title' => 'Existencia total', 'count' => 148, 'color' => 'info', 'icon' => 'fas fa-clipboard-list'],
             ['title' => 'Existencia vendida', 'count' => 33, 'color' => 'danger', 'icon' => 'fas fa-shopping-cart'],
             ['title' => 'Existencia actual', 'count' => 115, 'color' => 'primary', 'icon' => 'fas fa-chart-bar'],
@@ -17,7 +17,7 @@
             ['title' => 'Beneficio neto', 'count' => '$89', 'color' => 'cyan', 'icon' => 'fas fa-balance-scale']
         ];
 
-        foreach ($metrics as $metric) {
+        foreach ($metrics as $metric) { //automatic generation of metrics
             get_template_part('templates/card-metric', null, $metric);
         }
         ?>
